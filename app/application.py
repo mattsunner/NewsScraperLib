daba2d191ed28f76578849614274cd1da2825285
Matt Sunner <sunner.matt@gmail.com>
2020-09-27 21:26:15 -0500

    Document headlineGatherer()
    Generate docstring for function.

--- app/application.py
+++ app/application.py
@@ -13,6 +13,15 @@ import requests
 
 
 def headlineGatherer(url, tag):
+    """headlineGatherer: Function to return an array of headlines from a given URL and HTML tag.
+
+    Args:
+        url (str): URL for the site being used to gather headlines from.
+        tag (str): HTML tag for attribute(s) being gathered (eg.. 'h1', 'h2')
+
+    Returns:
+        array: Result of the function is an array of headline text gathered from the URL provided.
+    """
     response = requests.get(url)
     html_doc = response.text
     soup = BeautifulSoup(html_doc, 'lxml')
