fd0fe341e0501e0271db2d5a8d0602f5ec718539
Matt Sunner <sunner.matt@gmail.com>
2020-10-13 13:49:30 -0500

    Add Database Connection Test
    Add functional test and fixture for database connection in memory for future tests.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,24 @@
-from app import headlineGatherer
+from app import headlineGatherer, headlineStorer
+import sqlite3
+import pytest
+
+
+@pytest.fixture
+def database_setup():
+    """ Fixture to set up the in-memory database with test data """
+    conn = sqlite3.connect(':memory:')
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE headlines (headline text)''')
+
+    headline_sample = [
+        ('Sample Headline #1'),
+        ('Sample Headline #2')
+    ]
+
+    for item in headline_sample:
+        c.execute("INSERT INTO headlines(headline) VALUES(?)", (item,))
+    yield conn
 
 
 def test_headlineGatherer():
@@ -8,3 +28,14 @@ def test_headlineGatherer():
 
     # Assert that headlineGatherer returns a list
     assert headlineGatherer(url, tag, className) == []
+
+
+def test_connection(database_setup):
+    c = database_setup
+
+    assert len(list(c.execute('SELECT * FROM headlines'))) == 2
+
+
+# def test_headlineStorer():
+#     dbFilePath = pass
+#     dfHeadline = pass
